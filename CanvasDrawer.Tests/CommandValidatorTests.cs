dc1c03d5cc317ff4aa6145d84cfdb57b050e196c
Elias Martinez <elias.martinez@isolutions.ch>
2022-07-06 09:49:04 +0200

    Canvas command validation with tests implemented

--- CanvasDrawer.Tests/CommandValidatorTests.cs
+++ CanvasDrawer.Tests/CommandValidatorTests.cs
@@ -7,7 +7,43 @@ namespace CanvasDrawer.Tests
         private readonly CommandValidator commandValidator = new CommandValidator();
 
         [Fact]
-        public void CommandShouldBeValid()
+        public void EmptyCanvasCommandShouldNotBeValid()
+        {
+            var command = string.Empty;
+            var isValid = commandValidator.IsCanvasCommandValid(command);
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void NullCanvasCommandShouldNotBeValid()
+        {
+            string? command = null;
+            var isValid = commandValidator.IsCanvasCommandValid(command);
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void IncorrectCanvasCommandsShouldNotBeValid()
+        {
+            var commands = new[]
+            {
+                "C 20",
+                "C  30",
+                "A",
+                "C 20 -30",
+                "C -20 30",
+                "A 20 30"
+            };
+
+            foreach (var command in commands)
+            {
+                var isValid = commandValidator.IsCanvasCommandValid(command);
+                Assert.False(isValid);
+            }
+        }
+
+        [Fact]
+        public void CorrectCanvasCommandShouldBeValid()
         {
             var command = "C 20 30";
             var isValid = commandValidator.IsCanvasCommandValid(command);
--- CanvasDrawer/CommandValidator.cs
+++ CanvasDrawer/CommandValidator.cs
@@ -1,10 +1,18 @@
-﻿namespace CanvasDrawer
+﻿using System.Text.RegularExpressions;
+
+namespace CanvasDrawer
 {
     public class CommandValidator : ICommandValidator
     {
         public bool IsCanvasCommandValid(string? command)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(command))
+            {
+                return false;
+            }
+
+            var regex = new Regex(@"C \d+ \d+");
+            return regex.IsMatch(command);
         }
     }
 }

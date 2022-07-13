9a343600c38697387b023c0645bfa1dedc1e6756
Elias Martinez <elias.martinez@isolutions.ch>
2022-07-13 13:13:44 +0200

    Drawer tests fix

--- CanvasDrawer.Tests/DrawerTests.cs
+++ CanvasDrawer.Tests/DrawerTests.cs
@@ -1,8 +1,8 @@
 ﻿using CanvasDrawer.Console;
 using CanvasDrawer.Drawing;
+using CanvasDrawer.Models;
 using FakeItEasy;
 using System;
-using System.Drawing;
 using System.Text;
 using Xunit;
 
@@ -19,105 +19,130 @@ namespace CanvasDrawer.Tests
             drawer = new Drawer(fakeConsoleWriter);
         }
 
-        //[Fact]
-        //public void CanvasShouldBeDrawnCorrectly()
-        //{
-        //    var canvas = new Rectangle(1, 1, 4, 4);
-        //    var canvasDrawSb = new StringBuilder();
-        //    canvasDrawSb.AppendLine("------");
-        //    canvasDrawSb.AppendLine("|    |");
-        //    canvasDrawSb.AppendLine("|    |");
-        //    canvasDrawSb.AppendLine("|    |");
-        //    canvasDrawSb.AppendLine("|    |");
-        //    canvasDrawSb.AppendLine("------");
-        //    drawer.DrawCanvas(canvas);
+        [Fact]
+        public void CanvasShouldBeDrawnCorrectly()
+        {
+            var canvas = new Canvas(4, 4);
+            var canvasDrawSb = new StringBuilder();
+            canvasDrawSb.AppendLine("------");
+            canvasDrawSb.AppendLine("|    |");
+            canvasDrawSb.AppendLine("|    |");
+            canvasDrawSb.AppendLine("|    |");
+            canvasDrawSb.AppendLine("|    |");
+            canvasDrawSb.AppendLine("------");
+            drawer.Draw(canvas);
 
-        //    A.CallTo(() => fakeConsoleWriter.Write(canvasDrawSb.ToString())).MustHaveHappenedOnceExactly();
-        //}
+            A.CallTo(() => fakeConsoleWriter.Write(canvasDrawSb.ToString())).MustHaveHappenedOnceExactly();
+        }
 
-        //[Fact]
-        //public void DrawFigureShouldThrowAnExceptionIfCanvasWasNotInitialized()
-        //{
-        //    var figure = new Rectangle(1, 1, 4, 4);
-        //    Assert.Throws<InvalidOperationException>(() => drawer.Draw(figure));
-        //}
+        [Fact]
+        public void DrawLineShouldThrowAnExceptionIfCanvasWasNotInitialized()
+        {
+            var line = new Line(10, 10, 10, 20);
+            Assert.Throws<InvalidOperationException>(() => drawer.Draw(line));
+        }
 
-        //[Fact]
-        //public void ApplyBucketFillShouldThrowAnExceptionIfCanvasWasNotInitialized()
-        //{
-        //    var point = new Point(1, 1);
-        //    var color = 'c';
-        //    Assert.Throws<InvalidOperationException>(() => drawer.ApplyBucketFill(point, color));
-        //}
+        [Fact]
+        public void DrawRectangleShouldThrowAnExceptionIfCanvasWasNotInitialized()
+        {
+            var rectangle = new Rectangle(10, 10, 20, 20);
+            Assert.Throws<InvalidOperationException>(() => drawer.Draw(rectangle));
+        }
 
-        //[Fact]
-        //public void DrawFigureShouldBeDrawnCorretly()
-        //{
-        //    var canvas = new Rectangle(1, 1, 4, 4);
-        //    drawer.DrawCanvas(canvas);
-        //    Fake.ClearRecordedCalls(fakeConsoleWriter);
+        [Fact]
+        public void DrawBucketFillShouldThrowAnExceptionIfCanvasWasNotInitialized()
+        {
+            var bucketFill = new Line(10, 10, 10, 20);
+            Assert.Throws<InvalidOperationException>(() => drawer.Draw(bucketFill));
+        }
 
-        //    var figure = new Rectangle(1, 1, 2, 2);
-        //    var outputSb = new StringBuilder();
-        //    outputSb.AppendLine("------");
-        //    outputSb.AppendLine("|xxx |");
-        //    outputSb.AppendLine("|x x |");
-        //    outputSb.AppendLine("|xxx |");
-        //    outputSb.AppendLine("|    |");
-        //    outputSb.AppendLine("------");
+        [Fact]
+        public void DrawRectangleShouldBeDrawnCorretly()
+        {
+            var canvas = new Canvas(4, 4);
+            drawer.Draw(canvas);
+            Fake.ClearRecordedCalls(fakeConsoleWriter);
 
-        //    drawer.Draw(figure);
-        //    A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
-        //}
+            var rectangle = new Rectangle(1, 1, 3, 3);
+            var outputSb = new StringBuilder();
+            outputSb.AppendLine("------");
+            outputSb.AppendLine("|xxx |");
+            outputSb.AppendLine("|x x |");
+            outputSb.AppendLine("|xxx |");
+            outputSb.AppendLine("|    |");
+            outputSb.AppendLine("------");
 
-        //[Fact]
-        //public void BucketFillShouldBeAppliedCorretly()
-        //{
-        //    var canvas = new Rectangle(1, 1, 4, 4);
-        //    drawer.DrawCanvas(canvas);
+            drawer.Draw(rectangle);
+            A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
+        }
 
-        //    var figure = new Rectangle(1, 2, 2, 2);
-        //    drawer.Draw(figure);
-        //    Fake.ClearRecordedCalls(fakeConsoleWriter);
+        [Fact]
+        public void DrawLineShouldBeDrawnCorretly()
+        {
+            var canvas = new Canvas(4, 4);
+            drawer.Draw(canvas);
+            Fake.ClearRecordedCalls(fakeConsoleWriter);
 
-        //    var point = new Point(1, 1);
-        //    var color = 'c';
+            var line = new Line(1, 1, 3, 1);
+            var outputSb = new StringBuilder();
+            outputSb.AppendLine("------");
+            outputSb.AppendLine("|xxx |");
+            outputSb.AppendLine("|    |");
+            outputSb.AppendLine("|    |");
+            outputSb.AppendLine("|    |");
+            outputSb.AppendLine("------");
 
-        //    var outputSb = new StringBuilder();
-        //    outputSb.AppendLine("------");
-        //    outputSb.AppendLine("|cccc|");
-        //    outputSb.AppendLine("|xxxc|");
-        //    outputSb.AppendLine("|x xc|");
-        //    outputSb.AppendLine("|xxxc|");
-        //    outputSb.AppendLine("------");
+            drawer.Draw(line);
+            A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
+        }
 
-        //    drawer.ApplyBucketFill(point, color);
-        //    A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
-        //}
+        [Fact]
+        public void DrawBucketFillShouldBeDrawnCorrectly()
+        {
+            var canvas = new Canvas(4, 4);
+            drawer.Draw(canvas);
 
-        //[Fact]
-        //public void BucketFillToAFigureShouldBeAppliedCorretly()
-        //{
-        //    var canvas = new Rectangle(1, 1, 4, 4);
-        //    drawer.DrawCanvas(canvas);
+            var rectangle = new Rectangle(1, 2, 3, 4);
+            drawer.Draw(rectangle);
 
-        //    var figure = new Rectangle(1, 2, 2, 2);
-        //    drawer.Draw(figure);
-        //    Fake.ClearRecordedCalls(fakeConsoleWriter);
+            Fake.ClearRecordedCalls(fakeConsoleWriter);
 
-        //    var point = new Point(1, 2);
-        //    var color = 'c';
+            var bucketFill = new BucketFill(1, 1, 'c');
 
-        //    var outputSb = new StringBuilder();
-        //    outputSb.AppendLine("------");
-        //    outputSb.AppendLine("|    |");
-        //    outputSb.AppendLine("|ccc |");
-        //    outputSb.AppendLine("|c c |");
-        //    outputSb.AppendLine("|ccc |");
-        //    outputSb.AppendLine("------");
+            var outputSb = new StringBuilder();
+            outputSb.AppendLine("------");
+            outputSb.AppendLine("|cccc|");
+            outputSb.AppendLine("|xxxc|");
+            outputSb.AppendLine("|x xc|");
+            outputSb.AppendLine("|xxxc|");
+            outputSb.AppendLine("------");
 
-        //    drawer.ApplyBucketFill(point, color);
-        //    A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
-        //}
+            drawer.Draw(bucketFill);
+            A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
+        }
+
+        [Fact]
+        public void BucketFillShouldBeAppliedToAnotherColorCorrectly()
+        {
+            var canvas = new Canvas(4, 4);
+            drawer.Draw(canvas);
+
+            var rectangle = new Rectangle(1, 2, 3, 4);
+            drawer.Draw(rectangle);
+            Fake.ClearRecordedCalls(fakeConsoleWriter);
+
+            var bucketFill = new BucketFill(1, 2, 'c');
+
+            var outputSb = new StringBuilder();
+            outputSb.AppendLine("------");
+            outputSb.AppendLine("|    |");
+            outputSb.AppendLine("|ccc |");
+            outputSb.AppendLine("|c c |");
+            outputSb.AppendLine("|ccc |");
+            outputSb.AppendLine("------");
+
+            drawer.Draw(bucketFill);
+            A.CallTo(() => fakeConsoleWriter.Write(outputSb.ToString())).MustHaveHappenedOnceExactly();
+        }
     }
 }
